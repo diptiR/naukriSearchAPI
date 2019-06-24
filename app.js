056b5ed2658f40f8ccc6fc0c2470a5617ab53911
Dipti Redkar <redkar_d@India.XoriantCorp.com>
2019-06-24 23:35:46 +0530

    Implemented initial directory

---
+++ app.js
@@ -0,0 +1,73 @@
+var fs = require('fs');
+var express = require('express');
+const bodyParser = require("body-parser");
+var app = express();
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.send("HELLO WORLD");
+});
+let obj = [];
+function firstEntry(skill) {
+    obj.push(skill);
+    var json = JSON.stringify(obj);
+    fs.writeFile('skills.json', json, 'utf8', function callback(err, data) {
+        if (err) return console.error(err);
+    });
+}
+
+function appendEntry(skill) {
+    fs.readFile('skills.json', 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            firstEntry()
+            console.log(err);
+        } else {
+            obj = JSON.parse(data); //now it an object
+            obj.push(skill); //add some data
+            json = JSON.stringify(obj); //convert it back to json
+            fs.writeFile('skills.json', json, 'utf8', function callback(err, data) {
+                if (err) return console.error(err);
+            }); // write it back 
+        }
+    });
+}
+
+app.post('/create-skill', function (req, res) {
+    console.log(req.body);
+    fs.stat('myjsonfile.json', function (err, stat) {
+        if (err == null) {
+            // File exists
+            appendEntry(req.body);
+        } else if (err.code === 'ENOENT') {
+            // file does not exist
+            firstEntry(req.body);
+        } else {
+            console.log('Some other error: ', err.code);
+        }
+    });
+    res.end("yes");
+});
+
+app.listen(3000);
